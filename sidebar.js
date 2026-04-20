013fe47d4b7c7e530bb27ed9de5bc3005f3ddfb7
Swatantra Reddy <150887553+Swatant234@users.noreply.github.com>
2026-04-20 09:43:05 +0530

    checkin related to inward,

--- sidebar.js
+++ sidebar.js
@@ -37,6 +37,11 @@ const sidebarHTML = `
                     <div class="operations-group">
                         <a href="/inventory-budget/Inward_Inventory/a1.html" class="tree-link">Inward - Inventory</a>
                         <a href="/inventory-budget/Put_Away/a1.html" class="tree-link">Put Away</a>
+                        <a href="/inventory-budget/Request_for_Issuance/a1.html" class="tree-link">Request for Issuance</a>
+                        <a href="/inventory-budget/Issuance_of_Material/a1.html" class="tree-link">Issuance of Material</a>
+                        <a href="#" class="tree-link">Return Material</a>
+                   <a href="/inventory-budget/Stock_Adjustment/a1.html" class="tree-link">Stock Adjustment</a>
+                    <a href="/inventory-budget/Stock_Verification/a1.html" class="tree-link">Stock Verification</a>
                     </div>
                 </div>
             </div>
@@ -47,6 +52,25 @@ const sidebarHTML = `
         <span>BUDGET</span>
         <i class="bi bi-chevron-down ms-auto arrow-icon"></i>
     </a>
+
+    <div class="nav-item">
+    <a href="#purchaseCollapse" class="nav-link" data-bs-toggle="collapse" aria-expanded="true">
+        <i class="bi bi-cart-plus"></i> 
+        <span>PURCHASE</span>
+        <i class="bi bi-chevron-down ms-auto arrow-icon"></i>
+    </a>
+
+    <div class="collapse show" id="purchaseCollapse">
+        <div class="sub-menu-tree">
+            <div class="operations-group">
+                <a href="/inventory-budget/Purchase/purchase_request.html" class="tree-link">
+            
+                Purchase Request
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
 
     <div class="collapse show" id="budgetCollapse">
         <div class="sub-menu-tree">
